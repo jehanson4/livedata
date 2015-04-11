8163b684628322ae8dfc4a892ddd6173897139a3
jehanson4 <jehanson@gmail.com>
2015-04-11 11:05:37 -0400

    cleanup

--- org.jehanson.livedata/src/org/jehanson/livedata/LDListener.java
+++ org.jehanson.livedata/src/org/jehanson/livedata/LDListener.java
@@ -6,7 +6,14 @@ package org.jehanson.livedata;
  */
 public interface LDListener {
 
+	// TODO support for batch changes
+
+	/** experimental */
 	public void valueChanged(LiveData data);
+
+	/** experimental */
 	public void structureChanged(LiveData data);
+	
+	/** experimental */
 	public void referenceChanged(LiveData data);
 }
